0709b078931e99cf0fcf935d2a13cb0b840ad0d8
nouman-122333 <hafiznoumansaifi0000@gmail.com>
2024-09-15 01:57:18 -0700

    message

---
+++ gun/g.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+main()
+{
+system("color 02");
+cout<<"       +--^----------,--------,-----,--------^-,               "<<endl;
+cout<<"        | |||||||||   '--------'     |          O  "<<endl;
+cout<<"        '+---------------------------^----------|                   "<<endl;
+cout<<"          ',---------,--------,---------------'       "<<endl;
+cout<<"            / XXXXXX /'|       /'                     "<<endl;
+cout<<"           / XXXXXX /  |      /'                              "<<endl;
+cout<<"          / XXXXXX /'--------'                                           "<<endl;
+cout<<"         / XXXXXX /                                                      "<<endl;
+cout<<"        / XXXXXX /                                             "<<endl;
+cout<<"       (________(                                               "<<endl;
+cout<<"        '------'                                          "<<endl;
+}
---
+++ my game character/mgc.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+main()
+{
+system("COLOR 02"); 
+cout<<"              _________                                      "<<endl;
+cout<<"             |  _  _   |                 "<<endl;
+cout<<"             | |_||_|  |               "<<endl;
+cout<<"             | .____.  |                     "<<endl;
+cout<<"             |_________|                     "<<endl;
+cout<<"               /_____\\             "<<endl;
+cout<<"            _ | _____ |_               "<<endl;
+cout<<"           / /| \\   / |\\ \\                           "<<endl;
+cout<<"          /_/ |  \\_/  | \\_\\                                        "<<endl;
+cout<<"         /_/  |_______|  \\_\\                            "<<endl;
+cout<<"              |__|D|__|                                          "<<endl;
+cout<<"              | |   | |                "<<endl;
+cout<<"              | |   | |                 "<<endl;
+cout<<"              | |   | |                   "<<endl;              
+cout<<"             |___| |___|                          "<<endl;
+cout<<"                                      "<<endl;
+cout<<"               DR DOOM                            "<<endl;
+}
